d7a364d2c20b5d2f484703f485f689a57f49335c
sonorangirl <manahan.erin@gmail.com>
2016-12-04 15:06:22 -0700

    completed Flexbox Nav project, using gulp

---
+++ FlexBox/FlexBox Nav/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('styles', function() {
+	gulp.src('style.css')
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('style.css', ['styles']);
+});
